92bd8f10d671668a8059796f7a480a5a5ed834d5
progfay <shun1856shun@gmail.com>
2020-08-22 01:08:28 +0900

    feat: support args and return values

--- wiretap/wiretap.go
+++ wiretap/wiretap.go
@@ -1,9 +1,14 @@
 package wiretap
 
-import "sync/atomic"
+import (
+	"fmt"
+	"reflect"
+	"sync/atomic"
+)
 
 type Report struct {
 	callCount int64
+	fn        interface{}
 }
 
 func (r *Report) IncCount() {
@@ -14,10 +19,34 @@ func (r *Report) GetCount() int64 {
 	return atomic.LoadInt64(&r.callCount)
 }
 
-func Wiretap(f func()) (func(), *Report) {
+func Wiretap(dst, src interface{}) (*Report, error) {
+	vDst := reflect.ValueOf(dst)
+	vSrc := reflect.ValueOf(src)
+
+	if vDst.Kind() != reflect.Ptr {
+		return nil, fmt.Errorf("First argument of WiretapWithArgs must be pointer of function")
+	}
+
+	vDst = vDst.Elem()
+	if vDst.Kind() != reflect.Func {
+		return nil, fmt.Errorf("First argument of WiretapWithArgs must be pointer of function")
+	}
+
+	if vSrc.Kind() != reflect.Func {
+		return nil, fmt.Errorf("Second argument of WiretapWithArgs must be function")
+	}
+
+	if vDst.Type() != vSrc.Type() {
+		return nil, fmt.Errorf("Value of first argument must be same type function as second argument")
+	}
+
 	r := Report{callCount: 0}
-	return func() {
+	f := func(values []reflect.Value) []reflect.Value {
 		r.IncCount()
-		f()
-	}, &r
+		return vSrc.Call(values)
+	}
+
+	vDst.Set(reflect.MakeFunc(vDst.Type(), f))
+
+	return &r, nil
 }
